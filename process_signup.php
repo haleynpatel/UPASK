ffafb0edbb662b2bfc1c4dd61041ae057b11f0d8
Haleynp <haleynpatel@gmail.com>
2023-07-13 09:40:02 -0400

    Delete unecessary comments

--- process_signup.php
+++ process_signup.php
@@ -1,29 +1,15 @@
-
-
 <?php
-// Sign Up Page PHP
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    
-    // TODO: Process the form data (e.g., validate, store in database, etc.)
-    
-    // Connect to database
-    $conn = new mysqli('localhost', 'upask', 'upser' , 'upask');
-    $query = "INSERT INTO user(username, password, email)
-        VALUES ('$name', '$password', '$email');";
-        mysqli_query($conn,$query);
-
-    /* Display submitted data
-    echo "Name: " . $name . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Password: " . $password . "<br>";
-    */
-    
-    //Redirect to Success Page
-    header( 'Location: success.html' ) ;
-}
+    // Sign Up Page PHP
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve form data
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $conn = new mysqli('localhost', 'upask', 'upser' , 'upask');
+        $query = "INSERT INTO user(username, password, email)
+            VALUES ('$name', '$password', '$email');";
+            mysqli_query($conn,$query);
+        //Redirect to Success Page
+        header( 'Location: success.html' ) ;
+    }
 ?>
