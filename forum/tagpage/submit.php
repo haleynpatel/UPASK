112b3694a3dbdfc66cfddf70754a1e7e2a8c7651
Haleynp <haleynpatel@gmail.com>
2023-07-13 02:01:16 -0400

    Finished interface for user to add tags

--- forum/tagpage/submit.php
+++ forum/tagpage/submit.php
@@ -28,6 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   mysqli_close($connection);
 
   // Redirect or display a success message
-  echo "Form submitted successfully!";
+  echo "success";
+  header("Location: tagpagev3.php");
 }
 ?>
--- forum/tagpage/tagpagev3.php
+++ forum/tagpage/tagpagev3.php
@@ -94,19 +94,47 @@
   </style>
 </head>
 <body>
-  <form action="submit.php" method="post">
+<form action="submit.php" method="post">
     <label for="department">Department:</label>
     <input type="text" id="department" name="department">
     <div class="container">
-      <h1>Choose Your Skill:</h1>
+      <h2>Choose Your Skills:</h2>
       <div class="profile">
-        <input type="text" id="searchInput" placeholder="Search for coding languages and skills">
-        <div id="autocompleteContainer" class="autocomplete-container"></div>
+      <?php
+
+$servername = "localhost";
+$username = "upask";
+$password = "upser";
+$dbname = "upask";
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connection) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+// Fetch tags from the "tags" table
+$query = "SELECT * FROM tag";
+$result = mysqli_query($connection, $query);
+$tags = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
+<!-- HTML code for the dropdown menu -->
+<select name="tag">
+  <?php foreach ($tags as $tag) : ?>
+    <option value="<?php echo $tag['tag_id']; ?>"><?php echo $tag['tag_name']; ?></option>
+  <?php endforeach; ?>
+</select>
+
+
+<?php
+// Close the database connection
+mysqli_close($connection);
+?>
+
       </div>
-      <div id="selectedSkills" class="selected-skills"></div>
+      <div id="selectedSkills">
+        <input type="hidden" id="tagIds" name="tagIds" value="">
+      </div>
     </div>
-    <script src="script.js"></script>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Add">
   </form>
 </body>
 
